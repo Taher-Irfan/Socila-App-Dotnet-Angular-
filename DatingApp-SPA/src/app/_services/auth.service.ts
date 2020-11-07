0d239a00b71ddb483c4d930d136d7098083cf09a
Taher Irfan <taherhphs@gmail.com>
2020-11-08 01:02:55 +0600

    Clien side login done

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  baseurl = 'https://localhost:5001/api/auth/';
+
+  constructor(private http: HttpClient) {}
+  login(model: any) {
+    return this.http.post(this.baseurl + 'login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    );
+  }
+  register(model: any)
+  {
+    return this.http.post(this.baseurl + 'register', model);
+  }
+}
