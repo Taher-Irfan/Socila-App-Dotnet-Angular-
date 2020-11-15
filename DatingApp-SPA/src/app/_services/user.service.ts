1cb7a84cfcbbf2cc721045eb03c06a94b5fe06e1
Taher Irfan <taherhphs@gmail.com>
2020-11-16 00:35:21 +0600

    Module 9 done

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  baseurl = environment.apiUrl;
+  // jwtHelper = new JwtHelperService();
+  // decodedToken: any;
+  constructor(private http: HttpClient) {}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseurl + 'users');
+  }
+  getUser(id): Observable<User> {
+    return this.http.get<User>(this.baseurl + 'users/' + id);
+  }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -10,19 +10,60 @@ import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/ErrorInterceptor';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import {RouterModule} from '@angular/router';
-import {appRoutes} from './routes';
-
-
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { NgxGalleryModule } from 'ngx-gallery-9';
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, ListsComponent, MessagesComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule,RouterModule.forRoot(appRoutes), FormsModule, BsDropdownModule.forRoot()],
-  providers: [AuthService,
-    ErrorInterceptorProvider],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TabsModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    NgxGalleryModule,
+    BsDropdownModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: ['localhost:5001'],
+        disallowedRoutes: ['localhost:5001/api/auth'],
+      },
+    }),
+  ],
+  providers: [
+    AuthService,
+    MemberDetailResolver,
+    MemberListResolver,
+    UserService,
+    ErrorInterceptorProvider,
+
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
